9859033deb5dae2c13ddc0d309467bd61fee2c30
villosa1024 <villosa1024@gmail.com>
2022-06-11 05:37:33 +0900

    Add Euclid

--- main.cpp
+++ main.cpp
@@ -4,10 +4,50 @@
 
 #include<iostream>
 
-using namespace std;
+using Int = int;
+
+inline Int abs(Int a) {
+	return a >= 0 ? a : -1 * a;
+}
+
+
+Int euclid(Int a, Int b);
+
+
+
+
 
 int main(void) {
 
-    cout << "Hello World" << endl;
-    return 0;
+    
+	return 0;
+}
+
+Int euclid(Int a, Int b)
+{
+
+	Int gcd;
+	//gcd s.t >0
+
+
+	if (a == 0 && b == 0) {
+		gcd = -1;
+	}
+	else if (a == 0 && b != 0) {
+		gcd = abs(b);
+	}
+	else {
+		Int r0 = abs(a), r1 = abs(b);
+		while (r1 != 0) {
+			Int r2 = r0 % r1;
+			r0 = r1;
+			r1 = r2;
+		}
+
+
+		gcd = r0;
+	}
+	return gcd;
+
+
 }
